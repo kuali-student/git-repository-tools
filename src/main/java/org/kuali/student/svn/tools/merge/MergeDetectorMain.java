53365851376464ac99fcc5f9997caa0843924d1b
michael.ocleirigh <michael.ocleirigh@kuali.org>
2013-05-07 11:23:29 -0400

    KSENROLL-3907 Add first pass at merge detector
    The idea here is that we will read the dump file and notice when the
    copyfrom path prefix is different from the current path.
    
    And probably also require that the copyfrom path includes a "branch",
    "trunk" or "tag"
    
    git-svn-id: https://svn.kuali.org/repos/student/tools/svn-history-tools/trunk@47653 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ src/main/java/org/kuali/student/svn/tools/merge/MergeDetectorMain.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2013 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.kuali.student.svn.tools.merge;
+
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Map;
+
+import modifier.PathRevisionAndMD5AndSHA1;
+
+import org.eclipse.jgit.api.errors.DetachedHeadException;
+import org.kuali.student.svn.tools.AbstractParseOptions;
+import org.kuali.student.svn.tools.SvnDumpFilter;
+import org.kuali.student.svn.tools.merge.model.MergeDetectorData;
+import org.kuali.student.svn.tools.model.INodeFilter;
+import org.kuali.student.svn.tools.model.ReadLineData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Kuali Student Team
+ * 
+ * The idea for this search is that we should be able to deduce from the current path if the copy from path is different.
+ * 
+ * and if the copyfrom path contains "branch" or "tag" or "trunk" then there is a good change it was supposed to be a merge.
+ * 
+ *
+ */
+public class MergeDetectorMain {
+
+	private static final Logger log = LoggerFactory.getLogger(MergeDetectorMain.class);
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		if (args.length != 2) {
+			log.error("USAGE: <svn dump file> <merged paths output file>");
+			System.exit(-1);
+		}
+
+		try {
+
+			ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+					"svn/MergeDetector-applicationContext.xml");
+
+			applicationContext.registerShutdownHook();
+
+			SvnDumpFilter filter = applicationContext
+					.getBean(SvnDumpFilter.class);
+			
+			final PrintWriter pw = new PrintWriter(new FileOutputStream(args[1]));
+			
+			final MergeDetectorData detectorData = applicationContext
+					.getBean(MergeDetectorData.class);
+
+			filter.parseDumpFile(args[0], new AbstractParseOptions() {
+
+
+				private long currentRevision = 0L;
+				
+				
+				@Override
+				public void onRevision(long currentRevision,
+						ReadLineData lineData) {
+					
+					
+					if (this.currentRevision > 0)
+						detectorData.processRevision(pw, this.currentRevision);
+					
+					this.currentRevision = currentRevision;
+					
+
+				}
+
+				@Override
+				public void onStreamEnd(ReadLineData lineData) {
+					
+					if (this.currentRevision > 0)
+						detectorData.processRevision(pw, this.currentRevision);
+					
+				}
+
+				/*
+				 * (non-Javadoc)
+				 * 
+				 * @see org.kuali.student.svn.tools.AbstractParseOptions
+				 * #onAfterNode(long, java.lang.String, java.util.Map,
+				 * org.kuali.student.svn.tools.model .INodeFilter)
+				 */
+				@Override
+				public void onAfterNode(long currentRevision, String path,
+						Map<String, String> nodeProperties,
+						INodeFilter nodeFilter) {
+
+					String copyFromPath = nodeProperties
+							.get("Node-copyfrom-path");
+					
+					if (copyFromPath != null) {
+						
+						String copyFromRev = nodeProperties
+								.get("Node-copyfrom-rev");
+						
+						String copyFromMD5 = nodeProperties.get("Text-copy-source-md5");
+						
+						detectorData.storePath(Long.valueOf(copyFromRev), copyFromPath, copyFromMD5, currentRevision, path);
+						
+					}
+
+					
+				}
+
+			});
+
+		} catch (Exception e) {
+			log.error("Processing failed", e);
+		}
+
+
+		
+	}
+
+
+}
---
+++ src/main/java/org/kuali/student/svn/tools/merge/model/MergeDetectorDataImpl.java
@@ -0,0 +1,234 @@
+/*
+ * Copyright 2013 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.kuali.student.svn.tools.merge.model;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Kuali Student Team
+ *
+ */
+public class MergeDetectorDataImpl implements MergeDetectorData {
+
+	private static class TargetData {
+		
+		private Long revision;
+		private String path;
+		/**
+		 * @param revision
+		 * @param path
+		 */
+		public TargetData(Long revision, String path) {
+			super();
+			this.revision = revision;
+			this.path = path;
+		}
+		/**
+		 * @return the revision
+		 */
+		public Long getRevision() {
+			return revision;
+		}
+		/**
+		 * @return the path
+		 */
+		public String getPath() {
+			return path;
+		}
+		/* (non-Javadoc)
+		 * @see java.lang.Object#hashCode()
+		 */
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((path == null) ? 0 : path.hashCode());
+			result = prime * result
+					+ ((revision == null) ? 0 : revision.hashCode());
+			return result;
+		}
+		/* (non-Javadoc)
+		 * @see java.lang.Object#equals(java.lang.Object)
+		 */
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			TargetData other = (TargetData) obj;
+			if (path == null) {
+				if (other.path != null)
+					return false;
+			} else if (!path.equals(other.path))
+				return false;
+			if (revision == null) {
+				if (other.revision != null)
+					return false;
+			} else if (!revision.equals(other.revision))
+				return false;
+			return true;
+		}
+		/* (non-Javadoc)
+		 * @see java.lang.Object#toString()
+		 */
+		@Override
+		public String toString() {
+			StringBuilder builder = new StringBuilder();
+			builder.append("TargetData [revision=");
+			builder.append(revision);
+			builder.append(", path=");
+			builder.append(path);
+			builder.append("]");
+			return builder.toString();
+		}
+		
+		
+	}
+	
+	private static class SourceData extends TargetData {
+		
+		private String md5Hash;
+
+		/**
+		 * @param revision
+		 * @param path
+		 */
+		public SourceData(Long revision, String path, String md5Hash) {
+			super(revision, path);
+			
+			this.md5Hash = md5Hash;
+		}
+
+		/**
+		 * @return the md5Hash
+		 */
+		public String getMd5Hash() {
+			return md5Hash;
+		}
+
+		/* (non-Javadoc)
+		 * @see java.lang.Object#hashCode()
+		 */
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = super.hashCode();
+			result = prime * result
+					+ ((md5Hash == null) ? 0 : md5Hash.hashCode());
+			return result;
+		}
+
+		/* (non-Javadoc)
+		 * @see java.lang.Object#equals(java.lang.Object)
+		 */
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (!super.equals(obj))
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			SourceData other = (SourceData) obj;
+			if (md5Hash == null) {
+				if (other.md5Hash != null)
+					return false;
+			} else if (!md5Hash.equals(other.md5Hash))
+				return false;
+			return true;
+		}
+
+		/* (non-Javadoc)
+		 * @see java.lang.Object#toString()
+		 */
+		@Override
+		public String toString() {
+			StringBuilder builder = new StringBuilder();
+			builder.append("SourceData [md5Hash=");
+			builder.append(md5Hash);
+			builder.append(", getRevision()=");
+			builder.append(getRevision());
+			builder.append(", getPath()=");
+			builder.append(getPath());
+			builder.append("]");
+			return builder.toString();
+		}
+		
+		
+		
+	}
+	
+	private static class MergeData {
+		private SourceData copyFrom;
+		private TargetData targetData;
+		/**
+		 * @param copyFrom
+		 * @param targetData
+		 */
+		public MergeData(SourceData copyFrom, TargetData targetData) {
+			super();
+			this.copyFrom = copyFrom;
+			this.targetData = targetData;
+		}
+		
+		
+	}
+	
+	private List<MergeData>revisionMergeDataList = new ArrayList<>();
+	
+	/**
+	 * 
+	 */
+	public MergeDetectorDataImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see org.kuali.student.svn.tools.merge.model.MergeDetectorData#storePath(java.lang.Long, java.lang.String, java.lang.String, long, java.lang.String)
+	 */
+	@Override
+	public void storePath(Long copyFromRevision, String copyFromPath,
+			String copyFromMD5, Long currentRevision, String currentPath) {
+
+		SourceData sd = new SourceData(copyFromRevision, copyFromPath, copyFromMD5);
+		
+		TargetData td = new TargetData(currentRevision, currentPath);
+		
+		revisionMergeDataList.add(new MergeData(sd, td));
+		
+		
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.kuali.student.svn.tools.merge.model.MergeDetectorData#processRevision(java.io.PrintWriter, long)
+	 */
+	@Override
+	public void processRevision(PrintWriter outputWriter, Long currentRevision) {
+
+		//extract the merge path
+		
+		
+		revisionMergeDataList.clear();
+		
+	}
+
+}
