e5eedd5ba7a34605b1ae30a53415e179b2e9595e
michael.ocleirigh <michael.ocleirigh@kuali.org>
2013-06-07 16:05:23 -0400

    KSENROLL-3907 Add remote java debugging logic to allow remote testing on port 6523
    git-svn-id: https://svn.kuali.org/repos/student/tools/svn-history-tools/trunk@49450 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- scripts/copy-rename/filter-dump-files.sh
+++ scripts/copy-rename/filter-dump-files.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+DEBUG_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=6523"
+
 JAR_FILE=~/svn-history-tools-0.0.1-SNAPSHOT.jar 
 PIPE=/tmp/filter-dump-files.pipe
 DELETE=$1
+DEBUG=$2
 
 # delete any existing filtered dump files
 if test -n "$DELETE"
@@ -20,8 +23,15 @@ rm -f "$PIPE"
 
 mkfifo $PIPE
 
-echo "exec java -jar $JAR_FILE < $PIPE > java.log &"
-$(java -Xmx1024m -jar $JAR_FILE < $PIPE > java.log) &
+JAVA_DEBUG=""
+
+if test -n "$DEBUG" 
+then
+    JAVA_DEBUG="$DEBUG_OPTS"
+fi
+
+echo "exec java $JAVA_DEBUG -jar $JAR_FILE < $PIPE > java.log &"
+$(java $JAVA_DEBUG -Xmx1024m -jar $JAR_FILE < $PIPE > java.log) &
 
 JAVA_PID=$!
 
