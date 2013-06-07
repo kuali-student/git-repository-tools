a4babe8b9ddc3b13e2c193e97e1b3d865f9ae885
michael.ocleirigh <michael.ocleirigh@kuali.org>
2013-06-07 16:08:22 -0400

    KSENROLL-3907 alter history rewriter to accept work via stdin
    For the bulk rewrite we have 1292 dump files to process which is too big
    to be passed in as command line arguments.
    
    Now in the zero argument case the program will read line by line the join
    data file paths.
    
    git-svn-id: https://svn.kuali.org/repos/student/tools/svn-history-tools/trunk@49451 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- src/main/java/org/kuali/student/svn/tools/Main.java
+++ src/main/java/org/kuali/student/svn/tools/Main.java
@@ -15,9 +15,11 @@
  */
 package org.kuali.student.svn.tools;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Map;
 
@@ -30,129 +32,97 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * @author Kuali Student Team
- *
+ * 
  */
 public class Main {
 
 	private static final Logger log = LoggerFactory.getLogger(Main.class);
-	
+
+	private static void usage() {
+		log.error("USAGE: <join data file 1> [ <join data file 2> ... <join data file n>]");
+		log.error("We need a dump file for the target revision to exist in the current working directory in the format of rX.dump");
+		System.exit(-1);
+	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
-		if (args.length == 0) {
-			log.error("USAGE: <svn dump file version: 2 or 3> <join data file 1> [ <join data file 2> ... <join data file n>]");
-			log.error("For version 2 dump files we need a single dump file for the target revision");
-			log.error("For version 3 dump files we need a dump file for both the target and copyfrom revision.");
-			System.exit(-1);
-		}
-		
-		
-		int svnDumpFileVersion = Integer.parseInt(args[0]);
-		
 		try {
-			
-			ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("svn/applicationContext.xml");
-			
+
+			ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+					"svn/applicationContext.xml");
+
 			applicationContext.registerShutdownHook();
-			
-			INodeFilter nodeFilter = applicationContext.getBean(INodeFilter.class);
-			
-			for(int i = 1; i < args.length; i++) {
-				log.info(String.format ("Loading Join Data from (%s)", args[i]));
-				nodeFilter.loadFilterData(new File (args[i]));
-			}
-			
-			
-			List<JoinedRevision> joinWork = nodeFilter.getRevisionsToBeJoined();
-			
-			SvnDumpFilter filter = applicationContext.getBean(SvnDumpFilter.class);
-			
-			for (JoinedRevision joinedRevision : joinWork) {
-				
-				String targetDumpFilename = String.format("r%d.dump", joinedRevision.getTargetRevision());
-				
-				String filteredDumpFilename = String.format ("r%d-filtered.dump", joinedRevision.getTargetRevision());
-				
-				String copyFromDumpFilename = String.format("r%d.dump", joinedRevision.getCopyFromRevision());
-				
-				if (svnDumpFileVersion == 3) {
-					
-					/*
-					 * For version 3 dump's the md5 is on the diff content not the file content so we need to filter the copyfrom revision dump first
-					 * to extract the md5's of the diff content for each path being joined into the target revision.
-					 */
 
-					log.info(String.format(
-							"Extracting CopyFrom Hashes from (%s)",
-							copyFromDumpFilename));
+			INodeFilter nodeFilter = applicationContext
+					.getBean(INodeFilter.class);
 
-					filter.parseDumpFile(copyFromDumpFilename,
-							new AbstractParseOptions() {
+			if (args.length == 0) {
+				// read the list of files from stdin line by line
 
-								/*
-								 * (non-Javadoc)
-								 * 
-								 * @see
-								 * org.kuali.student.svn.tools.AbstractParseOptions
-								 * #onAfterNode(long, java.lang.String,
-								 * java.util.Map,
-								 * org.kuali.student.svn.tools.model
-								 * .INodeFilter)
-								 */
-								@Override
-								public void onAfterNode(long currentRevision,
-										String path,
-										Map<String, String> nodeProperties,
-										INodeFilter nodeFilter) {
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(System.in));
 
-									String sha1 = nodeProperties
-											.get("Text-delta-base-sha1");
+				try {
+					while (true) {
 
-									if (sha1 == null)
-										sha1 = nodeProperties
-												.get("Text-content-sha1");
+						String line = reader.readLine();
 
-									String md5 = nodeProperties
-											.get("Text-delta-base-md5");
+						if (line == null)
+							break;
 
-									if (md5 == null)
-										md5 = nodeProperties
-												.get("Text-content-md5");
+						log.info(String.format("Loading Join Data from (%s)",
+								line));
+						nodeFilter.loadFilterData(new File(line));
 
-									if (md5 != null) {
-
-										String fullPath = nodeFilter
-												.getFullCopyFromPath(
-														currentRevision, path);
-										nodeFilter.storeChecksumData(
-												currentRevision, fullPath,
-												sha1, md5);
-									}
-								}
-
-							}, nodeFilter);
-
+					}
+				} catch (IOException e) {
+					log.error("problem reading stdin", e);
 				}
-				
-				log.info(String.format("Started Joining (r%d) into (r%d)", joinedRevision.getTargetRevision(), joinedRevision.getCopyFromRevision()));
-				
-				filter.applyFilter(targetDumpFilename, filteredDumpFilename, nodeFilter);
-				
-				log.info(String.format("Finished Joining (r%d) into (r%d)", joinedRevision.getTargetRevision(), joinedRevision.getCopyFromRevision()));
-				
+			} else {
+
+				// read in the files from the command line
+				for (int i = 1; i < args.length; i++) {
+					log.info(String.format("Loading Join Data from (%s)",
+							args[i]));
+					nodeFilter.loadFilterData(new File(args[i]));
+				}
+
 			}
-			
-			
-			
+
+			List<JoinedRevision> joinWork = nodeFilter.getRevisionsToBeJoined();
+
+			SvnDumpFilter filter = applicationContext
+					.getBean(SvnDumpFilter.class);
+
+			for (JoinedRevision joinedRevision : joinWork) {
+
+				String targetDumpFilename = String.format("r%d.dump",
+						joinedRevision.getTargetRevision());
+
+				String filteredDumpFilename = String
+						.format("r%d-filtered.dump",
+								joinedRevision.getTargetRevision());
+
+				log.info(String.format("Started Joining (r%d) into (r%d)",
+						joinedRevision.getTargetRevision(),
+						joinedRevision.getCopyFromRevision()));
+
+				filter.applyFilter(targetDumpFilename, filteredDumpFilename,
+						nodeFilter);
+
+				log.info(String.format("Finished Joining (r%d) into (r%d)",
+						joinedRevision.getTargetRevision(),
+						joinedRevision.getCopyFromRevision()));
+
+			}
+
 		} catch (Exception e) {
 			log.error("Processing failed", e);
 		}
 
-
-		
 	}
-
 
 }
