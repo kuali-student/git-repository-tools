c211aa45afba29ef81ae22fd0963ac5884012fd3
michael.ocleirigh <michael.ocleirigh@kuali.org>
2013-06-13 14:12:23 -0400

    KSENROLL-3907 post svn cut-over commit
    git-svn-id: https://svn.kuali.org/repos/student/tools/svn-history-tools/trunk@49754 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- scripts/sync/sync-ks-to-staged.sh
+++ scripts/sync/sync-ks-to-staged.sh
@@ -3,16 +3,14 @@
 # helper script to keep things synced between the ks repo and the rewritten commit history repo.
 
 MODE=$1
-STAGED=$2
-SOURCE=$3
 
 usage () {
 
-	echo "USAGE: <mode: FETCH or APPLY>"
+	echo "USAGE: <mode: FETCH or APPLY> <mode options>"
 	echo "FETCH <target repo> <source repo>"
 	echo "target is where the start from rev comes from"
 	echo "source is where the rev's are coming from"
-	echo "APPLY <target repo> <dump file>"
+	echo "APPLY <target repo> <dump file> [user name] [password]"
 	exit 1
 
 }
@@ -45,6 +43,7 @@ then
 	# this is the next rev we need to get from the source repository
 	# echo $TARGET_REV
 
+	echo "svnrdump dump $SOURCE_REPO -r$TARGET_REV:HEAD --incremental > r$TARGET_REV-HEAD.dump"
 	svnrdump dump $SOURCE_REPO -r$TARGET_REV:HEAD --incremental > r$TARGET_REV-HEAD.dump
 
 	echo "CREATED Dump File: r$TARGET_REV-HEAD.dump"
@@ -53,12 +52,23 @@ elif test "$MODE" == "APPLY"
 then
 	TARGET_REPO=$2
 	DUMP_FILE=$3
+    USERNAME=$4
+    PASSWORD=$5
 
 	paramPresent $TARGET_REPO
 	paramPresent $DUMP_FILE
-	
-	echo "svnrdump load $TARGET_REPO < $DUMP_FILE"
-	svnrdump load $TARGET_REPO < $DUMP_FILE
+
+    CMD_OPTIONS=""
+ 
+    if test -n "$USERNAME"
+    then
+        paramPresent "$PASSWORD"
+
+        CMD_OPTIONS="--username $USERNAME --password $USERNAME"
+
+    fi        	
+	echo "svnrdump load $TARGET_REPO $CMD_OPTIONS < $DUMP_FILE"
+	svnrdump load $TARGET_REPO $CMD_OPTIONS < $DUMP_FILE
 
 else
 	echo "INVALID Mode: $MODE"
