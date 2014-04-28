a6c7e4ceef9c380735a6e4376f30d48734a3d8f1
michael.ocleirigh <michael.ocleirigh@kuali.org>
2014-04-28 19:53:08 -0400

    KSENROLL-11841 Add startup scripts
    scripts/run-svn-export.sh
    
    Run the svnadmin dump and bzip it.
    
    git-importer-student-plugin/scripts/run-installer.sh
    
    Run the student plugin configuring the classpath and accepting the range of commits in the dump file.
    
    Also support a debug mode which is useful for remote debugging on port 9996
    
    git-svn-id: https://svn.kuali.org/repos/student/tools/svn-history-tools/trunk@68784 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ git-importer-student-plugin/scripts/run-installer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+JAVA_CMD=/opt/jdk1.7.0_03/bin/java
+
+REV=$1
+
+DEBUG=$2
+
+DEBUG_OPTS=""
+
+if test -z $REV
+then
+	echo "USAGE: <start rev:end rev>"
+	exit 1
+fi
+
+if test -n $DEBUG
+then
+	DEBUG_OPTS="-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=9996,suspend=y"
+fi
+
+$JAVA_CMD -Xmx4096m -XX:MaxPermSize=512m $DEBUG_OPTS -cp "../../git-importer/target/git-importer-0.0.2-SNAPSHOT.jar:../target/git-importer-student-plugin-0.0.2-SNAPSHOT.jar" -Dspring.profiles.active=configured-plugin org.kuali.student.git.importer.GitImporterMain ~/ks-r$REV.dump.bz2 /home/mike/git/ks-git-bridge ks-r$REV-veto.log ks-r$REV-skipped-copy-from.log ks-r$REV-blob.log 1 https://svn.kuali.org/repos/student 0ac02d80-fb3f-11dc-a574-c57244a850eb /home/mike/bin/git > ks-r$REV-import.log
+
---
+++ scripts/run-svn-export.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+REPO=/home/mike/subversion/ks
+REV=$1
+
+if test -z $REV
+then
+	echo "USAGE: <start rev:end rev>"
+	exit 1
+fi
+
+svnadmin dump --incremental -r $REV $REPO | bzip2 > ks-r$REV.dump.bz2
