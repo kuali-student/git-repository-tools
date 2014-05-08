7d6445956ba34cfbf91616d74debe4b4c76b9943
michael.ocleirigh <michael.ocleirigh@kuali.org>
2014-05-08 16:55:23 -0400

    KSENROLL-11841 remove original svn-history-tools module
    Also use the kuali-common parent pom
    
    git-svn-id: https://svn.kuali.org/repos/student/tools/svn-history-tools/trunk@69241 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- svn-history-tools/scripts/sync/fetch-changes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#
-# fetch the changes between the rewriten and the source
-./sync-ks-to-staged.sh FETCH file:///home/mike/subversion/ksenroll-3907/staged-repo file:///home/mike/subversion/ks
